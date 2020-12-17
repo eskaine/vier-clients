137e143e6c424355baa0dde84a62dc3f7b890432
eskaine <eskaineliu@gmail.com>
2020-12-17 20:47:24 +0800

    fix auth

--- service/src/App.js
+++ service/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 import PrivateRoute from './shared/components/PrivateRoute';
 import socket from './shared/helpers/socket';
+import { getLocalAuth, setLocalAuth } from './shared/helpers/func';
 import Dashboard from './pages/Dashboard';
 import Kitchen from './pages/Kitchen';
 import Nav from './shared/components/Nav';
@@ -9,36 +10,41 @@ import Register from './shared/components/Register';
 import Login from './shared/components/Login';
 
 function App() {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(getLocalAuth());
 
-  // useEffect(() => {
-  //   socket.connect('service');
-  //   return () => {
-  //     socket.disconnect();
-  //   };
-  // }, []);
+  function setAuth(state) {
+    setIsAuth(state);
+    setLocalAuth(state);
+  }
+
+  useEffect(() => {
+    socket.connect('service');
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <div>
       {/* {isAuth ? <Nav /> : ''} */}
       <Nav />
       <Switch>
-        <Route exact path="/" >
-          {isAuth ? <Redirect to='/service' /> : <Login setIsAuth={setIsAuth} />}
+        <Route exact path="/">
+          {isAuth ? <Redirect to="/service" /> : <Login setIsAuth={setAuth} />}
         </Route>
         <Route exact path="/kitchen">
           <Kitchen />
           {isAuth ? <Kitchen /> : <Redirect to="/login" />}
         </Route>
         <Route path="/login">
-          {isAuth ? <Redirect to="/service" /> : <Login setIsAuth={setIsAuth} />}
+          {isAuth ? <Redirect to="/service" /> : <Login setIsAuth={setAuth} />}
           {/* <Login setIsAuth={setIsAuth} url="login" /> */}
         </Route>
         <Route path="/register">
-          {isAuth ? <Redirect to="/service" /> : <Register setIsAuth={setIsAuth} />}
+          {isAuth ? <Redirect to="/service" /> : <Register setIsAuth={setAuth} />}
           {/* <Register setIsAuth={setIsAuth} url="login" /> */}
         </Route>
-        <Route exact path="/service" >
+        <Route exact path="/service">
           <Dashboard />
           {/* {isAuth ? <Dashboard /> : <Redirect to="/login" />} */}
         </Route>
